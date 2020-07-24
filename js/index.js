4cd5738fef41547de2b897ec6a8aeeaf6986839f
Ascended <Ascended@mail.com>
2020-07-24 15:52:53 +0200

    Modal Options Template Setted

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@ const modal = $.modal({
     title: 'Modal',
     closable: true,
     content: `
-        <h4>Modal is working</h4>
         <p>Lorem ipsum dolor sit.</p>
     `,
     width: '400px'
