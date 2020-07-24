afec21facd3b2fdfa20fc6897838f1f30ba924cc
Ascended <Ascended@mail.com>
2020-07-24 17:00:01 +0200

    Footer Buttons Creating setted

--- js/index.js
+++ js/index.js
@@ -10,5 +10,16 @@ const modal = $.modal({
     content: `
         <p>Lorem ipsum dolor sit.</p>
     `,
-    width: '400px'
+    width: '400px',
+    footerButtons: [
+        {text: 'Ok', type: 'primary', handler() {
+            console.log('Primary btn clicked');
+            modal.close();
+        }},
+        {text: 'Cancel', type: 'danger', handler() {
+            console.log('Danger btn clicked');
+            modal.close();
+        }},
+        
+    ]
 })
--- js/plugins/modal.js
+++ js/plugins/modal.js
@@ -1,3 +1,30 @@
+Element.prototype.appendAfter = function (element) {
+  element.parentNode.insertBefore(this, element.nextSibling);
+}
+
+function noop() {}
+
+function _createModalFooter(buttons = []) {
+  if (buttons.length === 0) {
+    return document.createElement('div');
+  }
+
+  const wrap = document.createElement('div');
+  wrap.classList.add('modal-footer');
+
+  buttons.forEach(btn => {
+    const $btn = document.createElement('button');
+    $btn.textContent = btn.text;
+    $btn.classList.add(`btn-${btn.type || 'secondary'}`);
+    $btn.onclick = btn.handler || noop;
+    
+    wrap.appendChild($btn);
+  })
+
+
+  return wrap;
+}
+
 function _createModal(options) {
     const DEFAULT_WIDTH = '600px';
     const modal = document.createElement('div')
@@ -6,26 +33,26 @@ function _createModal(options) {
       "afterbegin",
       `
         <div class="modal-overlay" data-close="true">
-			<div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
+      <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
+      
 				<div class="modal-header">
 					<span class="modal-title">${options.title || 'Window'}</span>
 					${options.closable ? '<span class="modal-close" data-close="true">&times;</span>' : ''}
+        </div>
+        
+        <div class="modal-body" data-content>
+            ${options.content || ''}
 				</div>
-                <div class="modal-body" data-content>
-                    ${options.content || ''}
-				</div>
-				<div class="modal-footer">
-					<button>Ok</button>
-					<button>Cancel</button>
-				</div>
+				
 			</div>
 		</div>
     `
     )
+    const footer = _createModalFooter(options.footerButtons);
+    footer.appendAfter(modal.querySelector('[data-content]'))
+    document.body.appendChild(modal);
 
-    document.body.appendChild(modal)
-
-    return modal
+    return modal;
 }
 
 $.modal = function(options) {
