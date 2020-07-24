519089e168ee7143cb56d6971691dc428edcc1e3
Ascended <Ascended@mail.com>
2020-07-24 15:38:40 +0200

    Fruits Array List Created

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
+const fruits = [
+    {id: 1, title: 'Apples', price: 20, img: 'https://thelunchlady.ca/wp-content/uploads/2015/10/Apples-in-your-diet.jpg'},
+    {id: 2, title: 'Oranges', price: 30, img: 'https://www.twistedcitrus.co.nz/images/products/main/17_oranges.png'},
+    {id: 3, title: 'Mango', price: 40, img: 'https://st3.depositphotos.com/1020804/12760/i/450/depositphotos_127608560-stock-photo-mango-cubes-and-mango-fruit.jpg'},
+]
+
 const modal = $.modal()
