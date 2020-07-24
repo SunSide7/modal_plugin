c5dc3da5fd768ae64b308b7efbdd40f1d262a07f
Ascended <Ascended@mail.com>
2020-07-24 14:50:24 +0200

    Modal First Show Version created

--- js/plugins/modal.js
+++ js/plugins/modal.js
@@ -1,7 +1,39 @@
-$.modal = function() {
-    return {
-        open() {},
-        close() {},
-        destroy() {}
-    }
+function _createModal(options) {
+    const modal = document.createElement('div')
+    modal.classList.add('.vmodal')
+    modal.insertAdjacentHTML(
+      "afterbegin",
+      `
+        <div class="modal-overlay">
+			<div class="modal-window">
+				<div class="modal-header">
+					<span class="modal-title">Modal Title</span>
+					<span class="modal-close">&times;</span>
+				</div>
+				<div class="modal-body">
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing.</p>
+				</div>
+				<div class="modal-footer">
+					<button>Ok</button>
+					<button>Cancel</button>
+				</div>
+			</div>
+		</div>
+    `
+    )
+
+    document.body.appendChild(modal)
+
+    return modal
 }
+
+$.modal = function(options) {
+  const $modal = _createModal(options);
+
+  return {
+    open() {},
+    close() {},
+    destroy() {},
+  };
+};
